8eae477dc4e91ef2bfa43be4261f568b65123b4c
Abraham Medina <abrahammdnlpz@gmail.com>
2025-07-29 19:27:26 -0600

    Entregable 7.5.1: Proyecto Blog, Unificación de App en PostList y envoltura de App en Main

--- myBlog/src/App.jsx
+++ myBlog/src/App.jsx
@@ -1,25 +1,10 @@
 // Importación de etiquetas de envoltura, páginas y estilos
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import PostList from './pages/PostList'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import CreatePost from './pages/CreatePost'
 
 /* Creación de la función principal */
 function App() {
 
-  return (
-    <BrowserRouter>
-    {/* Rutas */}
-      <Routes>
-        <Route path='/' element={<PostList/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/create' element={<CreatePost/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <PostList />
 }
 
 export default App
